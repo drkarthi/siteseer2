52b270cd77bd2777a6c1fb9c84523de8e1ca1394
karthikthegoth <karthikthegoth@gmail.com>
2014-05-12 20:04:03 +0530

    Add search

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Siteseer::Application.config.secret_token = 'dfe7ae2436735798e511757a9fac06ca12412dfa136bf3a4922284e97d066c73f1e495e0b5051e355825537639c8e6aeddbfd6e4bf1f3ec67c021272f62130b7'
